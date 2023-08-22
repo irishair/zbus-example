d377175ba8817679462e247036519d7a724fb5f6
minsoo.kim <minsoo.kim@42dot.ai>
2023-08-22 15:21:33 +0900

    Update license
    Signed-off-by: minsoo.kim <minsoo.kim@42dot.ai>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 42dot All rights reserved.
+ * Copyright (c) 2023 42dot All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
